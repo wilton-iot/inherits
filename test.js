5c50d33ac59a9595cc66ce96d895162da84d5dba
alex <alex@staticlibs.net>
2017-05-25 22:24:27 +0100

    comvert tests to use assert

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
 var inherits = require('inherits/inherits.js')
-var t = require('tap')
+var t = require('assert')
 
 function test(c) {
   t.equal(c.constructor, Child)
   t.equal(c.constructor.super_, Parent)
   t.equal(Object.getPrototypeOf(c), Child.prototype)
   t.equal(Object.getPrototypeOf(Object.getPrototypeOf(c)), Parent.prototype)
-  t.isa(c, Child)
-  t.isa(c, Parent)
+  t.ok(c instanceof Child)
+  t.ok(c instanceof Parent)
 }
 
 function Child() {
@@ -23,6 +23,6 @@ inherits(Child, Parent)
 var c = new Child
 test(c)
 
-t.isa(inherits, 'function')
+t.ok(typeof(inherits) === 'function')
 
 return module.exports;});
